1c171447b4b985b2c83a029bd76e1ebec591b8ef
Marlon Prado - WINDOWS - GITHUB DESKTOP <marlonprado04@gmail.com>
2023-12-10 11:51:13 -0300

    refactor: remove module Menu

--- src/main.py
+++ src/main.py
@@ -1,25 +1,49 @@
-# Importando as classes
-from classes.Tarefa import Tarefa
-from classes.Projeto import Projeto
-from classes.Usuario import Usuario
 
-import Menus
+# Define função para exibir o menu principal
+def exibir_menu_principal():
+    print("\n----- MENU PRINCIPAL -----")
+    print("1. Entrar em tarefas")
+    print("2. Entrar em projetos")
+    print("3. Entrar em usuários")
+    print("4. Sair\n")
+    
+
+def exibir_menu_tarefa():
+    print("\n----- MENU DE TAREFAS -----")
+    print("1. Entrar em tarefas")
+    print("2. Entrar em projetos")
+    print("3. Entrar em usuários")
+    print("4. Sair\n")
+
+def exibir_menu_projeto():
+    print("\n----- MENU DE PROJETOS -----")
+    print("1. Entrar em tarefas")
+    print("2. Entrar em projetos")
+    print("3. Entrar em usuários")
+    print("4. Sair\n")
+
+def exibir_menu_usuario():
+    print("\n----- MENU DE USUÁRIOS -----")
+    print("1. Entrar em tarefas")
+    print("2. Entrar em projetos")
+    print("3. Entrar em usuários")
+    print("4. Sair\n")
 
 # Loop principal do programa
 while True:
-    Menus.exibir_menu_principal()
+    exibir_menu_principal()
 
     # Solicita a escolha do usuário
     escolha = input("\nEscolha uma opção (1-6): ")
 
     # Verifica a escolha do usuário
     if escolha == "1":
-        Menus.exibir_menu_tarefa()
+        exibir_menu_tarefa()
 
     elif escolha == "2":
-        Menus.exibir_menu_projeto()
+        exibir_menu_projeto()
 
     elif escolha == "3":
-        Menus.exibir_menu_usuario()
+        exibir_menu_usuario()
     else:
         print("\nOpção inválida. Por favor, escolha uma opção válida.\n")
