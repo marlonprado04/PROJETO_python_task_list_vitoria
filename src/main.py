0abfeffac730649f763015d834ee41ce98a812c8
Marlon Prado - WINDOWS - GITHUB DESKTOP <marlonprado04@gmail.com>
2023-12-10 12:41:53 -0300

    refactor: change menus

--- src/main.py
+++ src/main.py
@@ -10,40 +10,14 @@ def exibir_menu_principal():
 
 def exibir_menu_tarefa():
     print("\n----- MENU DE TAREFAS -----")
-    print("1. Entrar em tarefas")
-    print("2. Entrar em projetos")
-    print("3. Entrar em usuários")
+    print("1. Listar tarefas pendentes")
+    print("2. Listar tarefas concluídas ")
+    print("3. Atualizar status da tarefa")
+    print("3. Excluir tarefa")
     print("4. Sair\n")
 
 def exibir_menu_projeto():
     print("\n----- MENU DE PROJETOS -----")
-    print("1. Entrar em tarefas")
-    print("2. Entrar em projetos")
-    print("3. Entrar em usuários")
-    print("4. Sair\n")
 
 def exibir_menu_usuario():
     print("\n----- MENU DE USUÁRIOS -----")
-    print("1. Entrar em tarefas")
-    print("2. Entrar em projetos")
-    print("3. Entrar em usuários")
-    print("4. Sair\n")
-
-# Loop principal do programa
-while True:
-    exibir_menu_principal()
-
-    # Solicita a escolha do usuário
-    escolha = input("\nEscolha uma opção (1-6): ")
-
-    # Verifica a escolha do usuário
-    if escolha == "1":
-        exibir_menu_tarefa()
-
-    elif escolha == "2":
-        exibir_menu_projeto()
-
-    elif escolha == "3":
-        exibir_menu_usuario()
-    else:
-        print("\nOpção inválida. Por favor, escolha uma opção válida.\n")
